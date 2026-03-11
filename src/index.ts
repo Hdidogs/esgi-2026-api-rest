a933781de14d7fb75478ba6ebb2f306d0a4eb33b
Hdidogs <a.rohee@lprs.fr>
2026-03-11 12:11:51 +0100

    add init handlers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from "express"
+import {initHandlers} from "./handlers/routes.js";
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    return res.send({
-        message: "Hello World"
-    })
-})
+initHandlers(app);
 
 app.listen(PORT, () => {
     console.log("App is listening on port " + PORT)
